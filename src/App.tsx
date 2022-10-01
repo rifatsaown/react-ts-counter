1132f22ea377ef85fc35512b0f01be77a9d45588
rifatsaown <rifatsaown0@gmail.com>
2022-10-02 01:12:46 +0600

    countrer validation done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const increasing = () =>setCount(count+1);
+  const decreasing = () =>{
+    if (count>0) {
+      setCount(count-1);
+    }
+    else {alert("Value Can't be less than 0");}
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Count : {count}</h2>
+      <button onClick={decreasing} >Decreasing -</button>
+     <button onClick={increasing} >Increasing +</button>
+     
     </div>
   );
 }
+
 
 export default App;
