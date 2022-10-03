3d9a4f5f256aca75a79d7d35a218618da26cb650
rifatsaown <rifatsaown0@gmail.com>
2022-10-03 23:29:42 +0600

    Recap Counter app Ts

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,18 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  const increasing = () => setCount(count + 1);
-  const decreasing = () => {
-    if (count > 0) {
-      setCount(count - 1);
-    } else {
-      alert("Value Can't be less than 0");
+  const increaseCount =() => setCount(count+1);
+  const decreaseCount =() => {
+    if (count>0) {
+      setCount(count-1);
     }
-  };
+    else{alert("Only Positive Number Is alowed")}
+  }
   return (
     <div className="App">
-      <h2>Count : {count}</h2>
-      <button onClick={decreasing}>Decreasing -</button>
-      <button onClick={increasing}>Increasing +</button>
+      <h3>Count : {count}</h3>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
     </div>
   );
 }
