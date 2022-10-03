b96ffed311332259f2a2f4d9497b76ea20cd1ae6
rifatsaown <rifatsaown0@gmail.com>
2022-10-03 23:20:23 +0600

    Commit Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  const increasing = () =>setCount(count+1);
-  const decreasing = () =>{
-    if (count>0) {
-      setCount(count-1);
+  const increasing = () => setCount(count + 1);
+  const decreasing = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      alert("Value Can't be less than 0");
     }
-    else {alert("Value Can't be less than 0");}
-  }
+  };
   return (
     <div className="App">
       <h2>Count : {count}</h2>
-      <button onClick={decreasing} >Decreasing -</button>
-     <button onClick={increasing} >Increasing +</button>
-     
+      <button onClick={decreasing}>Decreasing -</button>
+      <button onClick={increasing}>Increasing +</button>
     </div>
   );
 }
-
 
 export default App;
